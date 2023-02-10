af5125d9e66cb1154defb7b2ab684af6afad5b31
AbenezerKb <aben.jacob3@gmail.com>
2023-02-10 06:24:32 +0300

    add multi head attention block and residual connection implementation

--- model.py
+++ model.py
@@ -67,3 +67,58 @@ class FeedForwardBlock(nn.Module):
         # (batch, seq_Len, d_model) --> (batch, seq_Len, d_ff)
         #  --> (batch, Seq_len, d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, dim_model: int, heads: int,
+                 dropout_prob: float) -> None:
+        super().__init__()
+        self.dim_model = dim_model // heads
+        self.dim_model = dim_model
+        self.heads = heads
+        self.query = nn.Linear(dim_model, dim_model)
+        self.key = nn.Linear(dim_model, dim_model)
+        self.value = nn.Linear(dim_model, dim_model)
+        self.output = nn.Linear(dim_model, dim_model)
+        self.dropout = nn.Dropout(dropout_prob)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        dim_key = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1))/math.sqrt(dim_key)
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0,
+                                                            float('-inf'))
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, query, key, value, mask):
+        query = self.query(query)
+        key = self.key(key)
+        value = self.value(value)
+
+        query = query.view(query.shape[0], query.shape[1],
+                           self.heads, self.dim_key).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.heads,
+                       self.dim_key).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1],
+                           self.heads, self.dim_key).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(
+            query, key, value, mask, self.dropout)
+        x = x.transpose(1, 2).contigous().view(x.shape[0], -1,
+                                               self.heads * self.dim_key)
+
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, features: int, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
